10d31639a2956a3b48dbfdb0ec4e66c5a0b2ce8f
taishi yoneta <tkw.yoneta@gmail.com>
2023-03-23 12:16:01 +0900

    add:lip

--- opencv001.py
+++ opencv001.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy
+import PIL
 
 # Video
 frameWidth = 640
@@ -7,12 +9,18 @@ frameHeight = 480
 #Video Source
 #cap = cv2.VideoCapture('videos/traffic.mp4') #自分のmp4のpathを入力
 cap = cv2.VideoCapture(0)
-
+cap_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+cap_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+size =(cap_width,cap_height)
+num_of_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+print(cap_width,cap_height,num_of_frame,fps)
 
 while True:
     ret, img = cap.read()
     img = cv2.resize(img, (frameWidth, frameHeight))
-    cv2.imshow('Video', img)
+    img2 = cv2.flip(img,1)
+    cv2.imshow('Video', img2)
     #print('ret=', ret)
 
     # qを押すと止まる。
